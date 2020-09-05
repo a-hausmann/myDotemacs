9fd71298ba051244056d820ac637cc9b2c5a1b23
Arnold Hausmann <arnoldh@comcast.net>
2020-09-05 01:17:23 -0400

    Add dwim for prettifying rule code (work).

--- local/aeh-useful.el
+++ local/aeh-useful.el
@@ -1,5 +1,5 @@
 ;; File name:     aeh-useful.el
-;; Last modified: Tue Dec 10, 2019 8:39:29
+;; Last modified: Sat Sep 05, 2020 1:14:57
 ;; Author:        Arnold Hausmann
 ;; Why:           This is where I will keep useful code fragments/functions.
 
@@ -8,19 +8,43 @@
   "Toggle narrowing."
   (interactive)
   (cond ((region-active-p)
-         ;; If region is highlighted, narrow to that
-         (call-interactively #'narrow-to-region)
-         (deactivate-mark t))
-        ((buffer-narrowed-p)
-         ;; Otherwise widen if narrowed
-         (widen))
-        ((derived-mode-p 'org-mode)
-         (call-interactively #'org-narrow-to-subtree))
-        (t
-         (message "Do not know what to narrow to.")
-         (call-interactively #'narrow-to-defun))))
+          ;; If region is highlighted, narrow to that
+          (call-interactively #'narrow-to-region)
+          (deactivate-mark t))
+    ((buffer-narrowed-p)
+      ;; Otherwise widen if narrowed
+      (widen))
+    ((derived-mode-p 'org-mode)
+      (call-interactively #'org-narrow-to-subtree))
+    (t
+      (message "Do not know what to narrow to.")
+      (call-interactively #'narrow-to-defun))))
 (global-set-key (kbd "C-c C-n") 'aeh/narrow-dwim)
 
+(defun aeh/make-pretty (p-from p-thru)
+  "Prettify Rule code by moving all and/or conjunctions to a new line"
+  (interactive "r")
+  (save-match-data
+    (save-excursion
+      (save-restriction
+        (let ((change-count 0))
+          (ff)
+          (goto-char p-from)
+          (while (re-search-forward "\\( and \\| or \\)" p-thru t )
+            (setq change-count (+ change-count 1))
+            (replace-match "
+\\1" nil nil))
+          (message (format "Made %d changes." change-count)))))))
 
+(defun aeh/prettify-rule-dwim ()
+  ""
+  (interactive)
+  (save-match-data
+    (save-excursion
+      (save-restriction
+        (let ((remove-count 0))
+          (cond ((region-active-p)
+                  (aeh/make-pretty (region-beginning) (region-end)))
+            (t (aeh/make-pretty (point-min) (point-max)))))))))
 
 (provide 'aeh-useful)
